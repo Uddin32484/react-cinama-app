0f0842b40692b4f385f4d489b7a55d402c94f001
Yub uddin <uddin32484>
2020-11-21 22:24:15 -0600

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <h1>this is staging</h1>
+      <header className="App-header">
+<h1>Welcome to  This is Yub! develop!!!! </h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
