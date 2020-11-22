cf3f075444dbc18c90c28606e65f864755380904
Yub uddin <uddin32484>
2020-11-21 22:12:58 -0600

    .

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ function App() {
   return (
    
     <div className="App">
-<<<<<<< HEAD
       <h1>Welcome to Yub Cinema 1111111</h1>
-=======
       <h1>this is staging</h1>
->>>>>>> origin/staging
     </div>
   );
 }
