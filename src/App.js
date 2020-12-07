4d1121ed24e53f5d77b7b516020f45231e80a4f5
Yub uddin <uddin32484>
2020-12-06 21:57:03 -0600

    added header component and style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import './App.css';
+// eslint-disable-next-line no-unused-vars
+import Header from './components/header/Header';
 import store from './redux/store';
-
 function App() {
   return (
  <Provider store={store}>
 <div className="app">
+    <Header/>
     <h1>Welcome to movie app</h1>
      <h2>Redux setup complete</h2>
     </div>
